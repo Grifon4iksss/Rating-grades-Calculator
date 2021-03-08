cf9ff77e557e20b476643b77ea5c5bc6bc156e0a
fen0day <39646556+Fen0day@users.noreply.github.com>
2021-03-08 13:33:03 +0200

    Update main.py
    Fix calculation

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-x = int(input("Enter the number of grades: "))
-y = 0
-b = 1
-g = 0
+numberGrades = int(input("Enter the number of grades: "))
+y = 0 # Sum of all grades
+b = 1 
 
 print()
 
-for h in range(x):
+for h in range(numberGrades):
 
     z = int(input("Type your " + str(b) + " grade here: "))
-    b = b + 1
+    b = b + 1 # show how many times was z called
     y = y + z
 
-y = (y + z) / x
+y = y / numberGrades # Calculation
+
 print()
 print("Your final grade is: " + str(y))
